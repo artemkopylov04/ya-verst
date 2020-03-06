3791766dc3c81c28ee14eb597a470a57556c9c9a
root <root@LAPTOP-B99M7UMQ.localdomain>
2020-03-06 17:04:21 +0300

    layouts

--- app.js
+++ app.js
@@ -6,29 +6,36 @@ const app = express();
 
 app.use('/static', express.static(`${__dirname}/css`));
 
-app.get('/details', (req, res) => {
+app.get('*', (req, res, next) => {
   const page = req.path.replace('/', '');
 
   const htmlPath = path.resolve(__dirname, 'html', `${page}.html`);
-
-  fs.readFile(htmlPath, (err, content) => {
-    if (err) {
-        res.send(err);
-    } else {
-        res.send(`
-            <!doctype html>
-            <html>
-                <head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-                <link rel="stylesheet" href="/static/index.css">
-                <meta http-equiv="refresh" content="5"></meta>
-                </head>
-                <body>
-                ${content}
-                </body>
-            </html>`);
-    }
-  });
+  
+  if (!fs.existsSync(htmlPath)) {
+    res.redirect('/start');
+  } else {
+    fs.readFile(htmlPath, (err, content) => {
+      if (err) {
+          next('unknowing error')
+      } else {
+          res.send(`
+        <!doctype html>
+        <html>
+            <head>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+              <link rel="stylesheet" href="/static/index.css">
+            </head>
+            <body>
+            ${content}
+            </body>
+        </html>`);
+      }
+    });
+  }
 });
+
+app.use((err, req, res) => {
+  res.send(err);
+})
 
 app.listen(3000, () => { console.log('Listening 3000 port'); });
