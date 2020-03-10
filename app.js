f7337553aa4a8c664db7aeb648e7d0fcb2915195
a <mail>
2020-03-10 13:17:54 +0300

    fixes media query, blocks, fix log block

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('*', (req, res, next) => {
         <!doctype html>
         <html>
             <head>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
               <link rel="stylesheet" href="/static/index.css">
             </head>
